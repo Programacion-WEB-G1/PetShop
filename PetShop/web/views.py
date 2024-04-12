eca974245946577720cae0336760d9e62c9f05c0
segajardor <se.gajardor@duocuc.cl>
2024-04-12 13:13:04 -0400

    Update views.py

--- PetShop/web/views.py
+++ PetShop/web/views.py
@@ -4,25 +4,25 @@ from django.shortcuts import render
 # Create your views here.
 
 def index (request):
-    return render (request,'index.html')
+    return render (request,'web/index.html')
 
 def gato (request):
-    return render (request,'gato.html')
+    return render (request,'web/gato.html')
 
 def perro (request):
-    return render (request,'perro.html')
+    return render (request,'web/perro.html')
 
 def carro (request):
-    return render (request,'carro.html')
+    return render (request,'web/carro.html')
 
 def iniciar (request):
-    return render (request,'iniciarsesion.html')
+    return render (request,'web/iniciarsesion.html')
 
 def perfil (request):
-    return render (request,'pefil.html')
+    return render (request,'web/pefil.html')
 
 def recupera (request):
-    return render (request,'recuperapass.html')
+    return render (request,'web/recuperapass.html')
 
 def user (request):
-    return render (request,'user_reg.html')
+    return render (request,'web/user_reg.html')
