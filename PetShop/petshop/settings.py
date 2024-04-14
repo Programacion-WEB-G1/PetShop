005925520823072878301cac4b953b5e0d9451dd
segajardor <se.gajardor@duocuc.cl>
2024-04-14 12:34:31 -0400

    Update settings.py

--- PetShop/petshop/settings.py
+++ PetShop/petshop/settings.py
@@ -76,11 +76,25 @@ WSGI_APPLICATION = 'petshop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.oracle',
+        'NAME': '127.0.0.1:1321/xe',
+        'USER': 'c##prueba',
+        'PASSWORD': 'prueba',
+        'TEST': {
+            'USER': 'default_test',
+            'TBLSPACE': 'default_test_tbls',
+            'TBLSPACE_TMP': 'default_test_tbls_tmp',
+        },
+    },
 }
 
 
