58c9ee0b6f1e9598cd052e7dd2da5cc81e4fd51f
segajardor <se.gajardor@duocuc.cl>
2024-04-14 21:21:14 -0400

    cambio

--- PetShop/web/admin.py
+++ PetShop/web/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Ciudad, Usuario, Producto, Venta
 
 # Register your models here.
+admin.site.register(Ciudad)
+admin.site.register(Usuario)
+admin.site.register(Producto)
+admin.site.register(Venta)
