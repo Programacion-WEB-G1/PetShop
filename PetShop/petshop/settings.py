b5a448b9623bf00c276fefd8ad0ee52a42c84d15
DanielA! <128544518+danywiwi@users.noreply.github.com>
2024-04-15 18:31:05 -0400

    intentando conectar

--- PetShop/petshop/settings.py
+++ PetShop/petshop/settings.py
@@ -9,9 +9,17 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import ssl
 from pathlib import Path
 
+import sys
+
+
+
+
+# import oracledb as cx_Oracle
+    
+    
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -76,10 +84,21 @@ WSGI_APPLICATION = 'petshop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.oracle',
+        'NAME': 'gfcefc463576e2a_vuw46hn5yet4fzcq_high.adb.oraclecloud.com',
+        'USER': 'PBD_002A',
+        'PASSWORD': 'Contrasenagrupo2',
+        'HOST': 'adb.sa-santiago-1.oraclecloud.com',
+        'PORT': '1522',
     }
 }
 
--- PetShop/web/models.py
+++ PetShop/web/models.py
@@ -1,3 +1,46 @@
+from pyexpat import model
 from django.db import models
 
+
 # Create your models here.
+class Categoria(models.Model):
+    nombre_categoria = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.nombre_categoria
+
+class Producto(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre_producto = models.CharField(max_length=200)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nombre_producto
+
+class Ciudad(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre_ciudad = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre_ciudad
+
+class Usuario(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    direccion = models.CharField(max_length=200)
+    ciudad = models.ForeignKey(Ciudad, on_delete=models.CASCADE)
+    correo = models.EmailField()
+    clave = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.nombre} {self.apellido}"
+
+class Venta(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=100)
+    direccion = models.CharField(max_length=200)
+    producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nombre
