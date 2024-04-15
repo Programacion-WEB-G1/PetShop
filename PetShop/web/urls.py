7c251f90758807f90852122eb57e1cdb211ff762
segajardor <se.gajardor@duocuc.cl>
2024-04-15 18:05:32 -0400

    mega cambios

--- PetShop/web/urls.py
+++ PetShop/web/urls.py
@@ -5,10 +5,10 @@ urlpatterns = [
     path('', index, name="index"),
     path('gato/', gato),
     path('carro/', carro),
-    path('producto/', producto),
+    path('producto/', producto, name="productos"),
     path('perro/', perro),
     path('recupera/', recupera),
     path('perfil/', perfil),
-    path('user/', user),
+    path('user/', user, name="user"),
     path('base/', base),
 ]
